7d228e3a610ce3efb457586d711ecdbbc84aec0e
AlonsoCampillos <Alonso.Campillos@alu.uclm.es>
2025-11-15 18:03:28 +0100

    Docker va?

--- Src/etl.py
+++ Src/etl.py
@@ -1,4 +1,3 @@
-import datetime
 import os
 
 import pandas as pd
@@ -7,16 +6,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-engine = create_engine("postgresql+psycopg2://{USER}:{PASSWORD}@127.0.0.1/postgres", echo=False)
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+engine = create_engine(f"postgresql+psycopg2://{user}:{password}@127.0.0.1/postgres", echo=False)
 
 def main():
+
+    with engine.connect() as conn:
+        conn.execute("CREATE SCHEMA IF NOT EXISTS bronze;")
+        conn.execute("CREATE SCHEMA IF NOT EXISTS silver;")
+        conn.execute("CREATE SCHEMA IF NOT EXISTS gold;")
+
     spotify_youtube = pd.read_csv("Dataset/Spotify_Youtube.csv")
     track_emotions = pd.read_csv("Dataset/Track_Emotions.csv")
     track_genres = pd.read_csv("Dataset/Track_Genres.csv")
 
-    spotify_youtube.to_sql(name='spotify_youtube', con=engine, if_exists='append')
-    track_emotions.to_sql(name='track_emotions', con=engine, if_exists='append')
-    track_genres.to_sql(name='track_genres', con=engine, if_exists='append')
+    spotify_youtube.to_sql(name='spotify_youtube', con=engine, schema='bronze', if_exists='replace', index=False)
+    track_emotions.to_sql(name='track_emotions', con=engine, schema='bronze', if_exists='replace', index=False)
+    track_genres.to_sql(name='track_genres', con=engine, schema='bronze', if_exists='replace', index=False)
 
 if __name__ == "__main__":
     main()
--- dockerfile
+++ dockerfile
@@ -4,7 +4,10 @@ WORKDIR /code
 
 COPY requirements.txt /code/requirements.txt
 COPY /Src/etl.py /code/etl.py
+COPY /Dataset/Spotify_Youtube.csv /code/Dataset/Spotify_Youtube.csv
+COPY /Dataset/Track_Emotions.csv /code/Dataset/Track_Emotions.csv
+COPY /Dataset/Track_Genres.csv /code/Dataset/Track_Genres.csv
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
-CMD ["python","etl.py"]
+CMD ["python3","./etl.py"]
