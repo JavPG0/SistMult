dd67da4b35bff333b766bdcc6fe1432df79814fb
AlonsoCampillos <Alonso.Campillos@alu.uclm.es>
2025-11-22 12:33:47 +0100

    Implementada la tier oro y arreglados algunos problemas con el preprocesamiento

--- Src/etl.py
+++ Src/etl.py
@@ -26,17 +26,24 @@ def main():
         conn.commit()
 
     spotify_youtube = pd.read_csv("Dataset/Spotify_Youtube.csv")
+    spotify_youtube["timestamp"] = [now] * len(spotify_youtube.index)
     track_emotions = pd.read_csv("Dataset/Track_Emotions.csv")
+    track_emotions["timestamp"] = [now] * len(track_emotions.index)
     track_genres = pd.read_csv("Dataset/Track_Genres.csv")
     track_genres["timestamp"] = [now] * len(track_genres.index)
 
-    spotify_youtube.to_sql(name='spotify_youtube', con=engine, schema='bronze', if_exists='replace', index=False)
-    track_emotions.to_sql(name='track_emotions', con=engine, schema='bronze', if_exists='replace', index=False)
-    track_genres.to_sql(name='track_genres', con=engine, schema='bronze', if_exists='replace', index=False)
+    spotify_youtube.to_sql(name='spotify_youtube', con=engine, schema='bronze', if_exists='append', index=False)
+    track_emotions.to_sql(name='track_emotions', con=engine, schema='bronze', if_exists='append', index=False)
+    track_genres.to_sql(name='track_genres', con=engine, schema='bronze', if_exists='append', index=False)
 
     preprocesador = Preproceso()
     unified = preprocesador.preprocesar(spotify_youtube, track_emotions, track_genres)
-    unified.to_sql(name='unified', con=engine, schema='silver', if_exists='replace', index=False)
+    unified["timestamp"] = [now] * len(unified.index)
+    unified.to_sql(name='unified', con=engine, schema='silver', if_exists='append', index=False)
+
+    gold = preprocesador.convertir_gold(unified)
+    gold["timestamp"] = [now] * len(gold.index)
+    gold.to_sql(name='gold', con=engine, schema='gold', if_exists='append', index=False)
 
 if __name__ == "__main__":
     main()
